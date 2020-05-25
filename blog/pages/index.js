ff5ae6c1bf67b33162722c2d3040e7839fae7830
Kate Green <kate@hellotill.com>
2020-05-25 15:20:43 -0400

    adding layout stuff

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,8 +1,20 @@
+import Layout from '../components/Layout';
+
 const Index = ({ title, description, ...props }) => {
-  return "<div>Hello, world!</div>";
+  return (
+    <Layout pageTitle={title}>
+      <h1 className="title">Kate Green</h1>
+      <p className="description">
+        {description}
+      </p>
+      <main>
+        Posts here.
+      </main>
+    </Layout>
+  )
 }
 
-export default Index
+export default Index;
 
 export async function getStaticProps() {
   const configData = await import('../config.json');
